6581401568261ef032d7cf7f96ea3b52c425a8bd
Nabi1711 <nabinabihandmade@gmail.com>
2026-07-03 22:54:35 +0630

    Update app.js

--- app.js
+++ app.js
@@ -32,3 +32,51 @@ function updateCartCount(){
 
 /* run on page load */
 updateCartCount();
+
+
+function copyOrder(){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    if(cart.length === 0){
+        alert("Cart is empty!");
+        return;
+    }
+
+    let text = "🧾 ORDER\n\n";
+    let total = 0;
+
+    cart.forEach(item=>{
+
+        let price = Number(item.price || 0);
+        let qty = Number(item.qty || 0);
+
+        let sum = price * qty;
+        total += sum;
+
+        text += ${item.name} x${qty} = ${sum} MMK\n;
+    });
+
+    text += \nTOTAL: ${total} MMK;
+
+    /* SAFE COPY METHOD */
+    if(navigator.clipboard && window.isSecureContext){
+        navigator.clipboard.writeText(text)
+        .then(()=> alert("Copied!"))
+        .catch(()=> fallbackCopy(text));
+    }else{
+        fallbackCopy(text);
+    }
+}
+
+/* fallback for mobile */
+function fallbackCopy(text){
+    let textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+
+    alert("Copied!");
+}
